d3b27c867348c02e8f768f55038e223996e67124
Samie Aji <ajisamie@gmail.com>
2026-08-08 21:28:39 +0100

    Update

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,30 +52,48 @@ public_users.get("/", async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function (req, res) {
   //Write your code here
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  try {
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error retrieving book",
+    });
+  }
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
   //Write your code here
-  const author = req.params.author;
-  let bookswithauthor = Object.values(books).filter(
-    (book) => book.author === author,
-  );
-  res.send(JSON.stringify(bookswithauthor, null, 4));
+  try {
+    const author = req.params.author;
+    let bookswithauthor = Object.values(books).filter(
+      (book) => book.author === author,
+    );
+    res.send(JSON.stringify(bookswithauthor, null, 4));
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error retrieving books",
+    });
+  }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
   //Write your code here
-  const title = req.params.title;
-  let bookswithtitle = Object.values(books).filter(
-    (book) => book.title === title,
-  );
-  res.send(JSON.stringify(bookswithtitle, null, 4));
+  try {
+    const title = req.params.title;
+    let bookswithtitle = Object.values(books).filter(
+      (book) => book.title === title,
+    );
+    res.send(JSON.stringify(bookswithtitle, null, 4));
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error retrieving books",
+    });
+  }
 });
 
 //  Get book review
